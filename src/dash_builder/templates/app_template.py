a379b42f33a79ce9f915e334cca7e507b07c0276
robfs <robfoxsimms@gmail.com>
2025-02-22 10:25:37 +0000

    add basic app template file

--- src/dash_builder/templates/app_template.py
+++ src/dash_builder/templates/app_template.py
@@ -4,14 +4,22 @@ __all__ = ["AppTemplate"]
 
 
 class AppTemplate:
-    @staticmethod
-    def content() -> str:
-        return """
-        import dash
+    @classmethod
+    def imports(cls) -> str:
+        return "import dash\n"
 
+    @classmethod
+    def body(cls) -> str:
+        return "app: dash.Dash = dash.Dash(__name__, use_pages=True)"
 
-        app = dash.Dash()
+    @classmethod
+    def main(cls) -> str:
+        return 'if __name__ == "__main__":\n\tapp.run(debug=True)\n'
 
-        if __name__ == "__main__":
-            app.run(debug=True)
-        """
+    @classmethod
+    def content_list(cls) -> list[str]:
+        return [cls.imports(), cls.body(), cls.main()]
+
+    @classmethod
+    def content(cls) -> str:
+        return "\n\n".join(cls.content_list())

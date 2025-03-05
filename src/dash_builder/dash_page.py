a4c3200413e735e07d707d818ebff16bb4ecfa64
robfs <robfoxsimms@gmail.com>
2025-03-05 19:02:55 +0000

    add docstring to `DashPage`

--- src/dash_builder/dash_page.py
+++ src/dash_builder/dash_page.py
@@ -9,6 +9,27 @@ __all__ = ["DashPage"]
 
 
 class DashPage(abc.ABC):
+    """Abstract class for defining pages in a Dash application.
+
+    # Example
+    ```python
+    import dash
+    from dash import html
+    from dash_builder import DashPage
+
+
+    dash.register_page(__name__, path="/")
+
+    class Homepage(Dash):
+        @classmethod
+        def valid_layout(cls, **kwargs):
+            return html.H1("This is the Homepage")
+
+    def layout(**kwargs):
+        return Homepage.layout(**kwargs)
+    ```
+    """
+
     @classmethod
     def error_container(cls, message):
         return html.Div(html.Pre(message))

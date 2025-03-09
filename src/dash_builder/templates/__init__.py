b7ed2951ff19c42f5fdbc6128cc54bae29173f4c
robfs <robfoxsimms@gmail.com>
2025-03-09 16:19:09 +0000

    delete old template files

--- src/dash_builder/templates/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Module containing the templates for building project files."""
-
-from .app_template import AppTemplate
-from .homepage_template import HomepageTemplate
-from .not_found_404_template import NotFound404Template
-
-__all__ = ["AppTemplate", "HomepageTemplate", "NotFound404Template"]

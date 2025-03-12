f257f2652dbc393830de1996af93a893f97e0636
robfs <robfoxsimms@gmail.com>
2025-03-12 19:12:20 +0000

    add page template

--- src/dash_builder/templates/__init__.py
+++ src/dash_builder/templates/__init__.py
@@ -1,5 +1,6 @@
 """Module containing templates for new object modules."""
 
+from .page_template import PageTemplate
 from .view_template import ViewTemplate
 
-__all__ = ["ViewTemplate"]
+__all__ = ["ViewTemplate", "PageTemplate"]
